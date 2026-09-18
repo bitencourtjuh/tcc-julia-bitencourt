f5a40c74c44a552caa9a0e9afbb33702c0249083
ArthrOliCode <arthur.rilber@gmail.com>
2026-09-18 14:30:53 -0300

    test: endpoints GET adicionados

--- paddleocr-mini/ocr-service/main.py
+++ paddleocr-mini/ocr-service/main.py
@@ -1,28 +1,69 @@
-from fastapi import FastAPI, UploadFile, File
+from fastapi import FastAPI, UploadFile, File, HTTPException
 from paddleocr import PaddleOCRClient
 import tempfile
+import traceback
+
 from pathlib import Path
 
-app = FastAPI()
+app = FastAPI(
+    title="Mini OCR API",
+    description="Uma API para extrair textos de documentos via PaddleOCR",
+    version="3.0.0"
+)
 
 client = PaddleOCRClient()
 
-@app.post("/ocr")
+@app.get("/")
+async def root():
+    return{
+        "message": "Mini API is running",
+        "version": "3.0.0"
+    }
 
+@app.get("/health")
+async def health_check():
+    return{
+        "statuts":"ok"
+    }
+
+@app.post("/ocr")
 async def process_document(file: UploadFile = File(...)):
+    
+    temporary_file_path = None
+    client = None
 
     # Arquivo temporário
-    with tempfile.NamedTemporaryFile(
-        delete=False,
-        suffix=".pdf"
-    )as temporary_file:
-        file_content = await file.read()
-
-        temporary_file.write(file_content)
-
-        temporary_file_path = temporary_file.name
+#    with tempfile.NamedTemporaryFile(
+#        delete=False,
+#        suffix=".pdf"
+#    )as temporary_file:
+#        file_content = await file.read()
+#
+#       temporary_file.write(file_content)
+#
+#        temporary_file_path = temporary_file.name
 
     try:
+        
+        if not file.filename:
+            raise HTTPException(
+                status_code=400,
+                detail="No filename was provided."
+            )
+
+        print(f"Received file: {file.filename}")
+        print(f"Content type: {file.content_type}")
+
+        allowed_extensions = {".pdf", ".png", ".jpeg", ".jpg"}
+
+        file_extension = Path(file.filename).suffix.lower()
+
+        if file_extension not in allowed_extensions:
+            raise HTTPException(
+                status_code=400,
+                detail=("Unsupported file type. "
+                        "Use pdf, jpg, jpeg, png")
+            )
         
         # Envio do pdf para o paddleOCR
         resultado = client.parse_document(
