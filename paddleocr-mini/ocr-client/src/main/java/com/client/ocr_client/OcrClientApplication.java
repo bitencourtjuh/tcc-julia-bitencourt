62a8581d821b9d611b9457cfa5a75843af968ac5
ArthurOliCode <arthur.rilber@gmail.com>
2026-09-23 16:24:29 -0300

    test: add start spring boot aplication

---
+++ paddleocr-mini/ocr-client/src/main/java/com/client/ocr_client/OcrClientApplication.java
@@ -0,0 +1,13 @@
+package com.client.ocr_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcrClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OcrClientApplication.class, args);
+	}
+
+}
---
+++ paddleocr-mini/ocr-client/src/test/java/com/client/ocr_client/OcrClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.client.ocr_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OcrClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
