8db319c2f1bca381a137e6122e694c5bff17f614
ArthurOliCode <arthur.rilber@gmail.com>
2026-09-25 20:16:53 -0300

    test: Add Service Layer

---
+++ paddleocr-mini/ocr-client/src/main/java/com/client/ocr_client/controller/DocumentController.java
@@ -0,0 +1,33 @@
+package com.client.ocr_client.controller;
+
+import com.client.ocr_client.service.OcrClient;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+
+@RestController
+@RequestMapping("/documents")
+public class DocumentController {
+
+    private final OcrClient ocrClient;
+
+    public DocumentController(OcrClient ocrClient) {
+        this.ocrClient = ocrClient;
+    }
+
+     @PostMapping("/ocr")
+    public String uploadDocument(@RequestParam("file") MultipartFile file) throws Exception {
+        return ocrClient.sendToOcr(file);
+    }
+
+    @GetMapping("/test")
+    public String status() {    
+        return "status: ok";
+    }
+}
+
---
+++ paddleocr-mini/ocr-client/src/main/java/com/client/ocr_client/service/OcrClient.java
@@ -0,0 +1,45 @@
+package com.client.ocr_client.service;
+
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class OcrClient {
+
+    private final RestClient restClient;
+
+    public OcrClient() {
+        this.restClient = RestClient.builder()
+                .baseUrl("http://127.0.0.1:8000")
+                .build();
+    }
+
+    public String sendToOcr(MultipartFile file) throws Exception {
+
+        ByteArrayResource resource =
+                new ByteArrayResource(file.getBytes()) {
+
+                    @Override
+                    public String getFilename() {
+                        return file.getOriginalFilename();
+                    }
+                };
+
+        MultiValueMap<String, Object> body =
+                new LinkedMultiValueMap<>();
+
+        body.add("file", resource);
+
+        return restClient.post()
+                .uri("/ocr")
+                .contentType(MediaType.MULTIPART_FORM_DATA)
+                .body(body)
+                .retrieve()
+                .body(String.class);
+    }
+}
